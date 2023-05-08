2d77605a920229556ea34175ddd2eb80860afd4a
fans_only <251115151@qq.com>
2023-05-08 10:10:29 +0800

    feat: add update version

---
+++ scripts/updateVersion.js
@@ -0,0 +1,24 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import minimist from "minimist";
+
+const argv = minimist(process.argv.slice(2));
+// console.log('argv: ', argv);
+
+const cwd = process.cwd();
+
+let newVersion = argv.version;
+
+const pkgJsonPath = path.resolve(cwd, "packages/vitamin-cli/package.json");
+const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath));
+const oldVersion = pkgJson.version;
+pkgJson.version = newVersion || genNewVersion(oldVersion);
+
+fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
+
+function genNewVersion(version) {
+  return version
+    .split(".")
+    .map((v, i, arr) => (i === arr.length - 1 ? +v + 1 : v))
+    .join(".");
+}
